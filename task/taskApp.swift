6cd3da3e94dda9c76cd8fed1ff8472659badba09
Olimdzhon <write2sd@mail.ru>
2021-10-23 13:06:45 +0500

    Initial Commit

---
+++ task/taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskApp.swift
+//  task
+//
+//  Created by Олимджон Садыков on 23.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
